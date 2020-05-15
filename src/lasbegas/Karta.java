a0015c2f6884701e4eb0288649a4d75e4d4b0d5b
Unknown <ArmageddonINcloud@wp.pl>
2020-05-15 19:02:35 +0200

    create Karta

---
+++ src/lasbegas/Karta.java
@@ -0,0 +1,26 @@
+package lasbegas;
+
+public class Karta {
+	private int wartosc;
+	private int kolor;
+	
+	/* A - 14
+	 * K - 13
+	 * Q - 12
+	 * J - 11
+	 * ----------------
+	 * Kier - 1
+	 * Pik - 2
+	 * Karo - 3
+	 * Trefl - 4
+	 */
+	public Karta(int w, int k) {
+		if((w > 14 || w < 2)||(k < 0 || k > 4)) {
+			wartosc = 0;
+			kolor = 0;
+			return;
+		}
+		wartosc = w;
+		kolor = k;
+	}
+}
