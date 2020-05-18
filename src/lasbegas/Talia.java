6eae6d205b193d9c7515169c54f922a85d19806f
Unknown <ArmageddonINcloud@wp.pl>
2020-05-18 18:06:54 +0200

    zmiany w talii
    dodatkowy typ na zwykłą pustą talię i zmiana z get na pull w pullRandomCard żeby sie nie jebało z getowymi funkcjami

--- src/lasbegas/Talia.java
+++ src/lasbegas/Talia.java
@@ -10,14 +10,15 @@ public class Talia {
 	private static Random rng;
 	
 	/*
-	 * 0 - standardowe 52 kart
+	 * 0 - pusta talia
+	 * 1 - standardowe 52 kart
 	 */
 	public Talia(int type) {
 		deck = new ArrayList();
 		rng = new Random();
-		Karta nowa;
 		
-		if(type == 0) {
+		if(type == 1) {
+			Karta nowa;
 			for(int i = 0; i < 4; i++)
 				for(int j = 1; j < 14; j++) {
 					nowa = new Karta(j+1, i+1);
@@ -26,14 +27,64 @@ public class Talia {
 		}
 	}
 	
+	public Karta getCard(int index) {
+		return deck.get(index);
+	}
+	
+	public int deckSize() { return deck.size(); }
+	
+	//potasowanie talii
 	public void shuffle() {
 		Collections.shuffle(deck);
 	}
 	
-	public Karta getRandomCard() {
+	//zwraca przypadkowa karte i usuwa ja z listy
+	public Karta pullRandomCard() {
+		if(deck.size() == 0)
+			return null;
 		int n = rng.nextInt(deck.size());
 		Karta rand = deck.get(n);
 		deck.remove(n);
 		return rand;
+	}
+	
+	//sumuje wartosci kart (na zasadach blackjacka)
+	public int sumUp(){
+		int war, wynik = 0, aces = 0;
+		for(int i = 0; i < deck.size(); i++) {
+			war = deck.get(i).getWartosc();
+			if(war == 14)
+				aces += 1;
+			else if(war > 10)
+				wynik += 10;
+			else
+				wynik += war;
+		}
+		
+		for(int i = 0; i < aces; i++) {
+			if(wynik + 11 <= 21)
+				wynik += 11;
+			else
+				wynik += 1;
+		}
+		
+		return wynik;
+	}
+	
+	
+	public void putCard(Karta k) {
+		if(k == null)
+			return;
+		deck.add(k);
+	}
+	
+	public String displayDeck() {
+		String wynik = "";
+		for(int i = 0; i < deck.size(); i++) {
+			wynik += deck.get(i).toString();
+			wynik += '|';
+		}
+		wynik += '\n';
+		return wynik;
 	}
 }
